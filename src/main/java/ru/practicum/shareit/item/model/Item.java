25c0b2d02448a557fb46ff23f8279c86a7d6b82e
Denis <dessitnov22@yandex.ru>
2026-01-11 16:35:04 +0300

    start project

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,16 @@
 package ru.practicum.shareit.item.model;
 
+import ru.practicum.shareit.request.ItemRequest;
+import ru.practicum.shareit.user.User;
+
 /**
  * TODO Sprint add-controllers.
  */
 public class Item {
+    Long id;
+    String name;
+    String description;
+    Boolean isAvailable;
+    User owner;
+    ItemRequest request;
 }
--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -4,4 +4,7 @@ package ru.practicum.shareit.user;
  * TODO Sprint add-controllers.
  */
 public class User {
+    Long id;
+    String name;
+    String email;
 }
